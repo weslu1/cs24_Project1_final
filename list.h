fe948daa6ffc96baf794f1630da251e5eca816e6
weslu1 <wesleychenlu@gmail.com>
2017-11-13 21:12:37 -0800

    Update list.h

--- list.h
+++ list.h
@@ -1,10 +1,19 @@
+
+#include "node.h"
+#include <iostream>
 using namespace std;
 
 class List{
 
  public:
 
+  void expressionToList(string exp);
+  node getHead();
 
  private:
+  node* head;
+};
 
-}
+
+
+
